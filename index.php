f808eca6f79577ad736d493abb0548a88bf0bdf1
Voxtagramix <ser9eevitch.pash@yandex.ru>
2022-05-12 15:00:09 +0300

    Test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $is_auth = rand(0, 1);
 $user_name = 'Paul'; // укажите здесь ваше имя
 $vid = array("Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты","Разное");
 $Info=[
-    ['Name'=>'2014 Rossignol District Snowboard', 'Cat'=>"$vid[0]", 'Price'=>'10999', 'URL'=>'img/lot-1.jpg'],
+    ['Name'=>'2014 Rossignol District Snowboard', 'Cat'=>"$vid[0]", 'Price'=>'10998', 'URL'=>'img/lot-1.jpg'],
     ['Name'=>'DC Ply Mens 2016/2017 Snowboard', 'Cat'=>"$vid[0]", 'Price'=>'159999', 'URL'=>'img/lot-2.jpg'],
     ['Name'=>'2014 Rossignol District Snowboard', 'Cat'=>"$vid[1]", 'Price'=>'8000', 'URL'=>'img/lot-3.jpg'],
     ['Name'=>'2014 Rossignol District Snowboard', 'Cat'=>"$vid[2]", 'Price'=>'10999', 'URL'=>'img/lot-4.jpg'],
